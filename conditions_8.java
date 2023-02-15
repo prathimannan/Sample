1e867628065dd2c610b5eaae5ba1fe67df1c4f6c
prathimannan <prathimannan143@gmail.com>
2023-02-15 12:03:30 +0530

    Condition1

--- conditions_8.java
+++ conditions_8.java
@@ -3,7 +3,7 @@ package conditions;
 public class conditions_8 {
 	public static void main(String[]args) {
 	int num = 24;
-	if (num%2 == 0 ) {
+	if (num%2 == 0 ) {           // update a code
 		System.out.println("even");
 	
 	}	else {
